530d8f6386cbdeb06392a33987b805fda89da0ce
mastiaha <mastiaha@mail.ru>
2022-01-05 23:02:34 +0300

    Добавьте файлы проекта.

---
+++ Задача 1/Linejnoe.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_1
+{
+    struct Linejnoe
+    {
+        double k;
+        double b;
+
+        public Linejnoe(double k, double b)
+        {
+            this.k = k;
+            this.b = b;
+        }
+        public string Root()
+        {
+            if (k == 0 && b == 0)
+                return "Корнем является любое число";
+            if (k == 0)
+                return "Корней нет";
+            double resultat1 = -b / k;
+            return $"Корень уравнения {resultat1}";
+            
+
+            
+            
+        }
+    }
+}
---
+++ Задача 1/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Задача_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите значение первого коэффициента");
+            double k = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Введите значение второго коэффициента");
+            double b = Convert.ToDouble(Console.ReadLine());
+            Linejnoe linejnoe = new Linejnoe(k, b);
+            Console.WriteLine(linejnoe.Root());
+            Console.ReadKey();
+        }
+    }
+}
